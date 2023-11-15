c0c3a310ccaa5aeec4445ba4a3a6548528c991a2
javca01 <103199318+javca01@users.noreply.github.com>
2023-11-14 22:17:50 -0500

    Add files via upload
    Agregamos el script para cargar los datos.

---
+++ scripts/data_acquisition/carga_data.py
@@ -0,0 +1,7 @@
+#Importamos la librerias necesarias.
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix
